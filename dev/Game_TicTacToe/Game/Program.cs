633d1a99a3d26a7b6773bb2352f47dd4193be56d
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-04-07 12:37:58 -0400

    added new file Game_TicTacToe
    [04/07/2021, 12:37] - added a Tic Tac Toe game that I built to this repo

---
+++ dev/Game_TicTacToe/Game/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+// Name: McKee, Jerry
+// Date: 02/03/2021
+// Course: APD
+// Synopsis: Game development code excercise, TicTacToe
+
+namespace Game
+{
+    class Program
+    {
+
+        // Application entry point
+        static void Main(string[] args)
+        {
+            bool play = true;
+            while (play)
+            {
+                //instantiate
+                TicTacToe.Play();
+
+                //Play Againg
+                play = PlayAgain();
+            }
+
+            //Create an exit message
+            Console.WriteLine("OK! This was fun");
+            Console.WriteLine("Can't wait to play again!");
+            Console.WriteLine("Have a great day!!");
+        }
+        //Allow the user to play again
+        private static bool PlayAgain()
+        {
+            Console.WriteLine("---------------------");
+            Console.Write("Would you like to play again? [Y/N]");
+
+            string response = Console.ReadLine().ToUpper();
+            return (response == "Y") ? true : false;
+        }
+
+
+
+    // End class
+    }
+}
